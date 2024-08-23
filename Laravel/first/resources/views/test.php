9556620e4287ec63867bd80f8b832c18f07b5299
Apex-Overlord-5 <114309468+Apex-Overlord-5@users.noreply.github.com>
2024-08-23 10:18:16 +0530

    yes i did

---
+++ Laravel/first/resources/views/test.php
@@ -0,0 +1,11 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome Page</title>
+</head>
+<body>
+    <h1>Welcome to Laravel!</h1>
+    <p>This is a simple view example.</p>
+</body>
+</html>
--- Laravel/first/routes/web.php
+++ Laravel/first/routes/web.php
@@ -30,4 +30,32 @@ Route::get('/practice', function () {
         echo "<strong>Question:</strong> " . $q['question'] . "<br>";
         echo "<strong>Answer:</strong> " . $q['answer'] . "<br><br>";
     }
+
+
+    Route::get('/products/{category}/{id}', function ($category, $id) {
+        return "Product $id in category $category";
+    })->where(['id' => '[0-9]+', 'category' => '[A-Za-z]+']);
+
+    Route::get('/products/{category}/{id}/{email}', function ($category, $id, $email) {
+        return "Product $id in category $category with email $email";
+    })->where([
+        'id' => '[0-9]+',
+        'category' => '[A-Za-z]+',
+        'email' => '[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}'
+    ]);
+
+    Route::get('/dob/{dob}', function ($dob) {
+        return "Date of Birth: $dob";
+    })->where('dob', '(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])-\d{2}');
+    Route::get('/values/{pi?}/{name?}', function ($pi = 3.14159, $name = "Rubesh") {
+        return "Value of Pi: $pi, Name: $name";
+    });
+    
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});    
+    
 });
+
+
